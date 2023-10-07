680e36717ee72b5f076a32c2805b12a76179d6eb
JaviBraus <javipicasjob@gmail.com>
2023-10-07 09:30:35 +0200

    Service, component, mock-db and interface created

---
+++ src/app/mock-db.ts
@@ -0,0 +1,8 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 1,  name: 'Spiderman'},
+    {id: 1,  name: 'Wonderwoman'},
+    {id: 1,  name: 'The Thing'},
+    {id: 1,  name: 'Miss Tornado'},
+]
