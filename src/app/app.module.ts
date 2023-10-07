086b5dac67e74668cb086555504b702b4c409ced
JaviBraus <javipicasjob@gmail.com>
2023-10-07 12:53:15 +0200

    Add hero works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
@@ -12,7 +12,8 @@ import { HeroComponent } from './hero/hero.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroServiceService } from '../hero-service.service';
+
+interface NewHero {
+  name: string;
+  superpower: string;
+}
 
 @Component({
   selector: 'app-hero',
@@ -7,4 +13,35 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
-}
+  showAddForm = false; 
+
+  newHero = { name: '', superpower: '' }
+
+  heroes = [
+    {id: 1,  name: 'Spiderman', superpower: 'Trepar como una araña'},
+    {id: 2,  name: 'Elastic girl', superpower: 'Elasticidad infinita'},
+    {id: 3,  name: 'The Thing', superpower: 'Fuerza sobrenatural'},
+    {id: 4,  name: 'Miss Tornado', superpower: 'Crear tornados'},
+  ];
+
+
+  toggleAddForm() {
+    this.showAddForm = !this.showAddForm;
+  }
+
+  onAdd() {
+    if (this.newHero.name && this.newHero.superpower) {
+      const newHero = {
+        id: this.heroes.length + 1,
+        name: this.newHero.name,
+        superpower: this.newHero.superpower
+      };
+      this.heroes.push(newHero);
+      this.newHero = { name: '', superpower: '' };
+      this.showAddForm = false;
+    }
+  }
+    }
+  
+
+
